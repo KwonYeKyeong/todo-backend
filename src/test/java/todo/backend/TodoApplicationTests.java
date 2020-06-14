22621a3143a84c18b918a7e5a746c40b3b78c92c
so3500 <dltjddhs@gmail.com>
2020-06-14 19:50:11 +0900

    init - init projects(gradle, dependencies, class)

---
+++ src/test/java/todo/backend/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package todo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
