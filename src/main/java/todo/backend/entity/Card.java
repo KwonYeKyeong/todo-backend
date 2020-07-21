272020dda0e4e2701b9d34e50a8a9ea2f552b1c3
KwonYeKyeong <kwonliz6.6@gmail.com>
2020-07-22 00:41:56 +0900

    할 일(title) 담당자(assignee) 우선순위(priority) 유효성 검증

--- src/main/java/todo/backend/entity/Card.java
+++ src/main/java/todo/backend/entity/Card.java
@@ -1,7 +1,6 @@
 package todo.backend.entity;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 public class Card implements Comparable{
@@ -12,10 +11,14 @@ public class Card implements Comparable{
 	@Size(max=20)
 	private String title;
 
+	@NotBlank
+	@Pattern(regexp="(^[a-zA-Z가-힣0-9]{1,15}+$)")
 	private String assignee;
 
 	private CardStatus status;
 
+	@Min(1)
+	@Max(3)
 	private Integer priority;
 
 	private LocalDate created = LocalDate.now();
