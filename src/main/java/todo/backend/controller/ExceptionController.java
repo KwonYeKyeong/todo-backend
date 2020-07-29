30885a05d483313a99de210ab9611555bea82f0d
KwonYeKyeong <kwonliz6.6@gmail.com>
2020-07-29 22:37:14 +0900

    bean validation을 벗어났을 때 발생하는 Exception을 처리하는 메서드 추가

--- src/main/java/todo/backend/controller/ExceptionController.java
+++ src/main/java/todo/backend/controller/ExceptionController.java
@@ -1,6 +1,7 @@
 package todo.backend.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,11 +13,15 @@ import todo.backend.exception.NotFoundException;
 @Slf4j
 public class ExceptionController {
 
-	@ExceptionHandler(NotFoundException.class)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public String handleValidException(NotFoundException e) {
-		//System.out.println(e.getClass());
+	public String handleBadRequestException(MethodArgumentNotValidException e) {
 		return e.getMessage();
 	}
 
+	@ExceptionHandler(NotFoundException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public String handleValidException(NotFoundException e) {
+		return e.getMessage();
+	}
 }
