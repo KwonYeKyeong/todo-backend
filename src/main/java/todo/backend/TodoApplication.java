376f9148f7c7b90bd4e38ec786e7e2f2b3930fa2
so3500 <dltjddhs@gmail.com>
2020-08-24 13:54:25 +0900

    chore: Remove unnecessary annotation

--- src/main/java/todo/backend/TodoApplication.java
+++ src/main/java/todo/backend/TodoApplication.java
@@ -3,10 +3,8 @@ package todo.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 @EnableJpaAuditing
 public class TodoApplication {
 
